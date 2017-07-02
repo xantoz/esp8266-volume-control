6d35729382a713801ca2c2061de0fe88b1a46113
Anton Kindestam <antonki@kth.se>
2017-07-02 22:54:14 +0200

    qt-gui/Protocol: Use shared_ptr to get rid of undefined behaviour in previous commit
    We essentially had the upwards funargs problem, by submitting a
    reference to the callback function to QTimer::singleShot, which had a
    reference to itself. Use a shared_ptr and heap allocation instead to
    solve this.

--- qt-gui/Protocol.cpp
+++ qt-gui/Protocol.cpp
@@ -307,34 +307,34 @@ void UdpProtocol::sendMsg(const char *msg)
     if (!isStatus)
     {
         // It should be fine to capture msgAry by value, as QByteArray:s are implicitly shared
-        std::function<void(void)> callback;
-        callback = [this, seqNr, msgAry, &callback, retries = 0u]() mutable {
-            if (seqNr <= this->largestReceivedAck) {
-                qDebug() << "SUCCESS: Command found ACK nicely: " << QString::fromLatin1(msgAry.data());
-                return;
-            }
-            if (seqNr < this->largestSentAck) {
-                qDebug() << "Not retrying because newer command sent: " << QString::fromLatin1(msgAry.data());
-                return;
-            }
-            if (retries > this->pingMissesBeforeDisconnect) {
-                qDebug() << "WARNING: Reached maximum retries for command: " << QString::fromLatin1(msgAry.data());
-                return;
-            }
+        auto callback = std::make_shared<std::function<void(void)>>();
+        *callback = std::move([this, seqNr, msgAry, callback, retries = 0u]() mutable {
+                if (seqNr <= this->largestReceivedAck) {
+                    qDebug() << "SUCCESS: Command found ACK nicely: " << QString::fromLatin1(msgAry.data());
+                    return;
+                }
+                if (seqNr < this->largestSentAck) {
+                    qDebug() << "Not retrying because newer command sent: " << QString::fromLatin1(msgAry.data());
+                    return;
+                }
+                if (retries > this->pingMissesBeforeDisconnect) {
+                    qDebug() << "WARNING: Reached maximum retries for command: " << QString::fromLatin1(msgAry.data());
+                    return;
+                }
 
-            // Retry
-            ++retries;
-            qDebug() << "RETRY (" << host << port << ")" << "UDP writeDatagram: " << QString::fromLatin1(msgAry.data());
-            socket->writeDatagram(msgAry, host, port);
-            QTimer::singleShot(this->retransmitDelay, Qt::PreciseTimer, this, callback);
-        };
+                // Retry
+                ++retries;
+                qDebug() << "RETRY (" << host << port << ")" << "UDP writeDatagram: " << QString::fromLatin1(msgAry.data());
+                socket->writeDatagram(msgAry, host, port);
+                QTimer::singleShot(this->retransmitDelay, Qt::PreciseTimer, this, *callback);
+            });
 
         /* TODO?: Use a timer attached to the class which we stop and change the callback to
            instead. This way we avoid calling old callbacks which have been converted to
            do-nothings. OTOH this way is easier converted to retransmitting always + in
            order semantics if we decide we want to replicate more of TCP in UDP (yey) in
            the future. */
-        QTimer::singleShot(this->retransmitDelay, Qt::PreciseTimer, this, callback);
+        QTimer::singleShot(this->retransmitDelay, Qt::PreciseTimer, this, *callback);
     }
 
     qDebug() << "(" << host << port << ")" << "UDP writeDatagram: " << QString::fromLatin1(msgAry.data());
